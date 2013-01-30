3e40223c5ee6f9d78b82cb1ca00a9ad80f1a49df
Daniel Tan <daniel.timothy.tan@gmail.com>
2013-01-30 11:36:18 -0600

    Here are my implementations of request, worker, and server.

---
+++ Request; Worker; Server/request.c
@@ -0,0 +1,12 @@
+#include "request_lsp_api.c"
+
+int main()
+{
+	/* Create request */
+	struct lsp_request* request = lsp_request_create("www.example.com",123);
+	string example = "new test";
+	int msg_cnt = 0;
+	lsp_request_write(request,example,msg_cnt);
+	/* Close the request when done */
+	lsp_request_close(request);
+}
---
+++ Request; Worker; Server/server.c
@@ -0,0 +1,22 @@
+#include "server_lsp_api.c"
+
+int main()
+{
+	/* Create server */
+	struct lsp_server* serv = lsp_server_create(123);
+
+	string input;
+	uint32_t fake_id = 0;
+	// int num_read = lsp_server_read(serv,(void*) &input, &fake_id);
+	int num_read;
+	while(true)
+	{
+		num_read = lsp_server_read(serv,(void*) &input, &fake_id);
+		if(num_read > 0)
+		{
+			printf("From Client: %s\n",input.c_str());
+		}
+	} 
+	/* Close the server when done */
+	// lsp_server_close(serv,1); //1 is just for testing needs to change
+}
